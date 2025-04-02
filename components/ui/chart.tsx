f0f15e7da8bb22d34f0091d46e04959da9da66fd
GimhanRanasinghe <gimhanravi@gmail.com>
2025-04-01 22:35:14 -0400

    inti

---
+++ components/ui/chart.tsx
@@ -0,0 +1,45 @@
+import {
+  BarChart as RechartsBarChart,
+  LineChart as RechartsLineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+  Bar,
+  Line,
+} from "recharts"
+
+interface ChartProps {
+  data: any[]
+  xField: string
+  yField: string
+  height: number
+  colors: string[]
+}
+
+export function BarChart({ data, xField, yField, height, colors }: ChartProps) {
+  return (
+    <ResponsiveContainer width="100%" height={height}>
+      <RechartsBarChart data={data}>
+        <XAxis dataKey={xField} />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey={yField} fill={colors[0]} />
+      </RechartsBarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export function LineChart({ data, xField, yField, height, colors }: ChartProps) {
+  return (
+    <ResponsiveContainer width="100%" height={height}>
+      <RechartsLineChart data={data}>
+        <XAxis dataKey={xField} />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey={yField} stroke={colors[0]} strokeWidth={2} />
+      </RechartsLineChart>
+    </ResponsiveContainer>
+  )
+}
+
